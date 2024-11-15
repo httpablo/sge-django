155773dc1aa8c28eb002e3860e3b857e8c34dab8
Pablo Brito <pablobrith@gmail.com>
2024-11-15 13:02:59 -0300

    CRUD de categories

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from . import forms
+from . import models
 
-# Create your views here.
+
+class CategoryListView(ListView):
+    model = models.Category
+    template_name = 'category_list.html'
+    context_object_name = 'categories'
+    paginate_by = 3
+
+    def get_queryset(self) :
+        query_set = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            query_set = query_set.filter(name__icontains=name)
+        return query_set
+    
+
+class CategoryCreateView(CreateView):
+    model = models.Category
+    template_name = 'category_create.html'
+    form_class = forms.CategoryForm
+    success_url = reverse_lazy('category_list')
+
+
+class CategoryDetailView(DetailView):
+    model = models.Category
+    template_name = 'category_detail.html'
+
+
+class CategoryUpdateView(UpdateView):
+    model = models.Category
+    template_name = 'category_update.html'
+    form_class = forms.CategoryForm
+    success_url = reverse_lazy('category_list')
+
+
+class CategoryDeleteView(DeleteView):
+    model = models.Category
+    template_name = 'category_delete.html'
+    success_url = reverse_lazy('category_list')
