2f348c9456728488c016e09be361756d5ca0b3de
httpablo <pablobrith@gmail.com>
2025-05-27 11:28:32 -0300

    agent  ai

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import json
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from ai.models import AIResult
 from . import metrics
 
 
@@ -12,6 +13,7 @@ def home(request):
     daily_sales_quantity_data = metrics.get_daily_sales_quantity_data()
     graphic_product_category_metric = metrics.get_graphic_product_category_metric()
     graphic_product_brand_metric = metrics.get_graphic_product_brand_metric()
+    ai_result = AIResult.objects.first().result
 
     context = {
         'product_metrics': product_metrics,
@@ -20,5 +22,6 @@ def home(request):
         'daily_sales_quantity_data': json.dumps(daily_sales_quantity_data),
         'product_count_by_category': json.dumps(graphic_product_category_metric),
         'product_count_by_brand': json.dumps(graphic_product_brand_metric),
+        'ai_result': ai_result,
     }
     return render(request, 'home.html', context)
